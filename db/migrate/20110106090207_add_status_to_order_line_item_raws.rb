2c54e4fcd41f4d95f32675c48739d6fbbe66b599
lijian <lijian@lijianmatoMacBook-Pro.local>
2011-01-07 04:13:49 +0800

    move migration to init_sql

--- db/migrate/20110106090207_add_status_to_order_line_item_raws.rb
+++
@@ -1,9 +0,0 @@
-class AddStatusToOrderLineItemRaws < ActiveRecord::Migration
-  def self.up
-    add_column :order_line_item_raws,:status,:integer,:default=>0
-  end
-
-  def self.down
-    remove_column :order_line_item_raws,:status
-  end
-end
--- db/migrate/20110106175658_add_address_to_salesrep.rb
+++
@@ -1,9 +0,0 @@
-class AddAddressToSalesrep < ActiveRecord::Migration
-  def self.up
-    add_column :salesreps,:address,:string
-  end
-
-  def self.down
-    remove_column :salesreps,:address
-  end
-end
