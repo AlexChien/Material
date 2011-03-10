1fc0ed3392fa88d634bab0a2c8c20cb05cf8c310
lijian <lijian@lijianmatoMacBook-Pro.local>
2011-03-10 14:16:00 +0800

    rc salesrep addres

--- app/models/salesrep.rb
+++ app/models/salesrep.rb
@@ -2,26 +2,26 @@ class Salesrep < ActiveRecord::Base
   belongs_to :region
   has_many :transfer_line_items
   has_many :order_line_item_raws
-  
-  validates_presence_of :name
-  
+
+  validates_presence_of :name, :address
+
   named_scope :in_region, lambda {|region_id|
         {:conditions => ["salesreps.region_id in (?)", region_id]}
   }
-  
+
   named_scope :in_state, lambda {|state|
         {:conditions => ["salesreps.state = ?", state]}
   }
-  
+
   state_machine :initial => :activated do
     event :delete_salesrep  do transition all => :deleted end
   end
-  
+
   before_save :add_region
-  
+
 protected
   def add_region
     self.region = User.current_user.region
   end
-  
+
 end
